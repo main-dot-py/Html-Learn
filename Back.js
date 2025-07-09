4aac03b245bf81b6600a3d88070edd0fcdbbf91b
Main.py <134731345+main-dot-py@users.noreply.github.com>
2025-07-09 22:28:54 +0200

    added stuff

---
+++ Back.js
@@ -0,0 +1,6 @@
+// Select the button by its ID
+const Back = document.getElementById('BackButton');
+// Add a click event listener
+Back.addEventListener('click', () => {
+  window.location.href = 'index.html';
+});
---
+++ Button.js
@@ -0,0 +1,6 @@
+// Select the button by its ID
+const ClickMe = document.getElementById('ClickMe');
+// Add a click event listener
+ClickMe.addEventListener('click', () => {
+  window.location.href = 'https://www.youtube.com/watch?v=xvFZjo5PgG0';
+});
